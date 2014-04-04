c290cd24503d6c2ceb407c18468e3b68584cf484
Ryan2013 <b9807014@mail.ntust.edu.tw>
2014-04-04 13:35:47 +0800

    auto yes install and python install

--- basic_installer.sh
+++ basic_installer.sh
@@ -1,9 +1,25 @@
-#update first 
+#update first
+echo "update......."
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
-# install the guake terminal that could use F12 to start the terminal 
-sudo apt-get install guake
+
+# install the guake terminal that could use F12 to start the terminal
+echo "install guake......."
+sudo apt-get -y install guake
+
 # install the boost lib 
-sudo apt-get install libboost-all-dev
+echo "install boost....."
+sudo apt-get -y install libboost-all-dev
+
 # install the photo edit
-sudo apt-get install gimp
+echo "install image editor gimp"
+sudo apt-get -y install gimp
+
+#install the screenshots editor
+echo "install shutter"
+sudo apt-get -y install shutter
+
+echo "install octave"
+sudo apt-get -y install octave
+
+
 
--- python_install.sh
+++ python_install.sh
@@ -1,4 +1,4 @@
-# pyenv environment setting refers to https://github.com/yyuu/pyenv.git
+#pyenv environment setting refers to https://github.com/yyuu/pyenv.git
 cd 
 git clone git://github.com/yyuu/pyenv.git .pyenv
 #add pyenv to bashrc,the guide can be refer to above
@@ -6,6 +6,15 @@ echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 exec $SHELL
 pyenv install 2.7.5
 pyenv rehash
+
+#install the numpy
+sudo apt-get install python-dev build-essential
+sudo pip install numpy
+
+#install the pip https://docs.google.com/document/d/1YNw5oaM8itWiGt2GH03g0v7UHsIvLBcMDuEsP5IQ-Jg/edit
+curl -O http://python-distribute.org/distribute_setup.py
+sudo python distribute setup.py
+sudo easy install pip 
 
 
 
