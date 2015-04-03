5ad2277da494aee69ab03472ebb704b40a277438
Ryan2013 <b9807014@mail.ntust.edu.tw>
2015-04-03 13:15:34 +0800

    updated opencv installation

--- openCvBuilder.sh
+++ openCvBuilder.sh
@@ -1,23 +1,12 @@
-#These are libraries and tools required by opencv
-sudo apt-get install build-essential checkinstall cmake pkgconfig yasm
-
-#For image
-sudo apt-get install libtiff4-dev libjpeg-dev libjasper-dev
-
-#for video I/O
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
-
-#for python
-sudo apt-get install python-dev python-numpy
-#other third-party libraries
-sudo apt-get install libtbb-dev
-#for GUI
-sudo apt-get install libqt4-dev libgtk2.0-dev
-#for configuaration of opencv
-tar -xvf opencv-2.4.6.1.tar.gz
-cd opencv-2.4.6.1
+#Remove all opencv dependence
+sudo find / -name "*opencv*" -exec rm -i {} \;
+git clone https://github.com/Itseez/opencv.git ~/software/ 
+cd ~/software/opencv/
 mkdir build
 cd build
-#Can watch README to select which feature do u want
-cmake -D WITH_QT=ON -D WITH_XINE=ON -D WITH_OPENGL=ON -D WITH_TBB=ON -D BUILD_EXAMPLES=ON
-
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUDA=ON -D WITH_CUBLAS=ON -D CUDA_FAST_MATH:BOOL=1 ..
+make -j8
+sudo checkinstall
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf"'
+sudo ldconfig
+echo "Install complete"
